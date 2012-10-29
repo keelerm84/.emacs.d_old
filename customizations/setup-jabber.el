eda0a98b6fc06dff7a94e1fb4ded4ed58ed2754f
Matthew M. Keeler <keelerm84@gmail.com>
2012-10-28 21:56:23 -0500

    Fixing jabber faces

--- customizations/setup-jabber.el
+++ customizations/setup-jabber.el
@@ -1,9 +1,23 @@
-'(jabber-activity-personal-face (:foreground "deep pink" :weight bold))
-'(jabber-chat-prompt-foreign (:foreground "salmon" :weight bold))
-'(jabber-chat-prompt-local (:foreground "olive drab" :weight bold))
-'(jabber-rare-time-face (:foreground "LightGoldenrod3" :underline t))
-'(jabber-roster-user-away (:foreground "peru" :slant italic :weight normal))
-'(jabber-roster-user-online (:foreground "dark orange" :slant normal :weight bold))
+(set-face-foreground 'jabber-activity-personal-face "deep pink")
+(set-face-bold-p 'jabber-activity-personal-face t)
+
+(set-face-foreground 'jabber-chat-prompt-foreign "salmon")
+(set-face-bold-p 'jabber-chat-prompt-foreign t)
+
+(set-face-foreground 'jabber-chat-prompt-local "olive drab")
+(set-face-bold-p 'jabber-chat-prompt-local t)
+
+(set-face-foreground 'jabber-rare-time-face "LightGoldenrod3")
+(set-face-underline-p 'jabber-rare-time-face t)
+
+(set-face-foreground 'jabber-roster-user-away "peru")
+(set-face-italic-p 'jabber-roster-user-away t)
+(set-face-bold-p 'jabber-roster-user-away nil)
+
+(set-face-foreground 'jabber-roster-user-online "dark orange")
+(set-face-italic-p 'jabber-roster-user-online nil)
+(set-face-bold-p 'jabber-roster-user-online t)
+
 (setq jabber-roster-line-format "%c %-25n %u %-8s  %S")
 
 (add-hook 'jabber-chat-mode-hook (lambda() (flyspell-mode t)))
