3142397057df5eb655852a1196c1560b63c96335
Matthew M. Keeler <keelerm84@gmail.com>
2012-12-23 23:57:18 -0600

    Additional improvements to snippets
    + Fix buffer after expansion

--- customizations/setup-yasnippet.el
+++ customizations/setup-yasnippet.el
@@ -4,5 +4,8 @@
        '("~/.emacs.d/snippets/" "~/.emacs.d/el-get/yasnippet/snippets/"))
 (mapc 'yas/load-directory yas/root-directory)
 (yas/global-mode)
+(setq yas/indent-line nil)
+
+(add-hook 'yas/after-exit-snippet-hook 'clean-up-buffer-or-region)
 
 (provide 'setup-yasnippet)
--- defuns/buffer-defuns.el
+++ defuns/buffer-defuns.el
@@ -12,3 +12,15 @@
     (switch-to-buffer (get-buffer-create bufname))
     (emacs-lisp-mode)
     ))
+
+(defun clean-up-buffer-or-region ()
+  "Untabifies, indents and deletes trailing whitespace from buffer or region."
+  (interactive)
+  (save-excursion
+    (unless (region-active-p)
+      (mark-whole-buffer))
+    (untabify (region-beginning) (region-end))
+    (indent-region (region-beginning) (region-end))
+    (save-restriction
+      (narrow-to-region (region-beginning) (region-end))
+      (delete-trailing-whitespace))))
