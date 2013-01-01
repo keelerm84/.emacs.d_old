dada9efde9ebf654b833c3609c14bc85d1df613c
Matthew M. Keeler <keelerm84@gmail.com>
2013-01-01 17:27:42 -0600

    Add support for .zip to dired

--- my-misc.el
+++ my-misc.el
@@ -5,4 +5,9 @@
 
 (set-default 'indent-tabs-mode nil)
 
+
+(eval-after-load "dired-aux"
+   '(add-to-list 'dired-compress-file-suffixes
+                 '("\\.zip\\'" ".zip" "unzip")))
+
 (provide 'my-misc)
