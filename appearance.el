73be29041650156e18594b67690ab7f74eb62d74
Matthew M. Keeler <keelerm84@gmail.com>
2013-01-01 17:56:39 -0600

    Enabling show paren mode

--- appearance.el
+++ appearance.el
@@ -33,4 +33,27 @@
 (set-face-foreground 'font-lock-comment-delimiter-face "bisque3")
 (set-face-italic-p 'font-lock-comment-delimiter-face nil)
 
+;; Show paren
+
+(defadvice show-paren-function
+  (after show-matching-paren-offscreen activate)
+  "If the matching paren is offscreen, show the matching line in the
+        echo area. Has no effect if the character before point is not of
+        the syntax class ')'."
+  (interactive)
+  (if (not (minibuffer-prompt))
+      (let ((matching-text nil))
+        ;; Only call `blink-matching-open' if the character before point
+        ;; is a close parentheses type character. Otherwise, there's not
+        ;; really any point, and `blink-matching-open' would just echo
+        ;; "Mismatched parentheses", which gets really annoying.
+        (if (char-equal (char-syntax (char-before (point))) ?\))
+            (setq matching-text (blink-matching-open)))
+        (if (not (null matching-text))
+            (message matching-text)))))
+
+(show-paren-mode 1)
+(setq show-paren-delay 0)
+(set-face-attribute 'show-paren-match-face nil :weight 'extra-bold)
+
 (provide 'appearance)
